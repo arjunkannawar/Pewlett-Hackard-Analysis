598a8e0f4d4522afe40d61bdf3c15bb07e0f9564
arjunkannawar <76499898+arjunkannawar@users.noreply.github.com>
2021-02-14 03:15:53 -0500

    adding assignment files

---
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,51 @@
+--Deliverable 1
+--Part 1: The Number of Retiring Employees by Title
+SELECT e.emp_no, 
+	   e.first_name,
+	   e.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+--Part 2: Remove the duplicates and keep only the most recent title of each employee
+SELECT DISTINCT ON (emp_no)
+		emp_no,
+		first_name,
+		last_name,
+		title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--Part 3: Retrieve the number of employees by their most recent job title who are about to retire
+SELECT count(title), 
+		title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(title) DESC;
+
+--Deliverable 2
+--Query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON(e.emp_no)
+	   e.emp_no, 
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
